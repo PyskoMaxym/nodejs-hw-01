ac5d913ec75bbcbede882613bd129743319f8c58
PyskoMaxym <pisko.maks@gmail.com>
2025-02-25 09:35:57 +0200

    fixed faked

--- src/utils/createFakeContact.js
+++ src/utils/createFakeContact.js
@@ -1,11 +1,13 @@
 import { faker } from "@faker-js/faker";
 
 function createFakeContact ()  {
-  id: faker.string.uuid(),
-  name: faker.person.fullName(),
-  phone: faker.phone.number(),
-  email: faker.internet.email(),
-  job: faker.person.jobTitle(),
+  return {
+    id: faker.string.uuid(),
+    name: faker.person.fullName(),
+    phone: faker.phone.number(),
+    email: faker.internet.email(),
+    job: faker.person.jobTitle(),
+  }
 };
 
 module.exports = { createFakeContact };
