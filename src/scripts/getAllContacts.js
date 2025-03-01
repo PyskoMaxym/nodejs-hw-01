0534d9de75d3cb8f140209782824e5ed9b6fd370
PyskoMaxym <pisko.maks@gmail.com>
2025-03-01 18:39:06 +0200

    fix get all

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,7 +2,7 @@ import  readContacts  from "../utils/readContacts.js";
 async function getAllContacts ()  {
 
     try{
-        const contacts = await readContacts || [];
+        const contacts = await readContacts();
         console.log('Contacts', contacts);
         return contacts;
     } catch (error){
